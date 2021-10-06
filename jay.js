db2a661528bd5fca20dc3142649a8eedb6fa5a2f
microwizard21 <90038222+microwizard21@users.noreply.github.com>
2021-10-06 01:32:09 -0800

    Add files via upload

---
+++ jay.js
@@ -0,0 +1,27 @@
+ function add(a,b){
+   return a + b
+ }
+
+ function subtract(a,b){
+   return a - b
+ }
+ 
+ function divide(a,b){
+   return a / b
+ }
+
+ function multiply(a,b){
+   return a * b
+  }
+
+  function modulo(a,b){
+    return a / b
+  }
+
+  console.log(add(5,2))
+  console.log(subtract(7,2))
+  console.log(divide(6,3))
+  console.log(multiply(2,8))
+  console.log(modulo(8,2))
+
+ 
